c2044824725c991c5f660e3ace7be2b3e8b9808c
Gabriel Sousa <32619617+itsgabrielsousa@users.noreply.github.com>
2019-10-10 20:50:30 -0300

    Add functions verificarVazio() and modificarEstiloVazio()
    These functions are responsible for changing the styles of the cards when they are empty

--- js/script.js
+++ js/script.js
@@ -13,14 +13,14 @@ function addTarefa() {
     // Criar elementos
     const li = document.createElement('li')
 
-    // Add classe ao novo elemento de lista
-    li.classList.add('card-list-item')
-
     // Add texto a lista
     li.textContent = inputTarefa.value
 
     // Append a nova li a ul do card todo
     document.querySelector('#card-todo ul').appendChild(li)
+
+    // origem eh null pois nao existe um card de origem vindo do input
+    verificarVazio(null, document.getElementById('card-todo'))
 
     inputTarefa.value = ''
 }       
@@ -64,5 +64,51 @@ function transitarTarefa() {
     else if (cardTargetIdName == 'card-done') {
         // remover li do card de origem
         ulParent.removeChild(liTarget)
+    }
+    
+    // verificar se os cars de origem e destino precisam ser estilizados caso estejam vazios
+    verificarVazio(cardTarget, cardDestino)
+}
+
+// Reponsavel por checar se alguma mudanca de estilo precisa ser feita,
+// ele deve ser chamado toda vez que uma tarefa for adicionada, ou transitada de um card para o outro, ou removida
+function verificarVazio(origem, destino) {
+    // encontrar os ul de card origem e de destino
+    let ulOrigParent = null
+    let ulDestParent = null
+
+    // tentar recurperar a UL dos cards de Origem e Destino, se por acaso algum deles nao existir, definir como null
+    try { ulOrigParent = origem.getElementsByClassName('card-list-container')[0] } 
+    catch (error) { ulOrigParent = null }
+
+    try { ulDestParent = destino.getElementsByClassName('card-list-container')[0] } 
+    catch (error) { ulDestParent = null }
+
+    // verificar se cada card vai ser estilizado ou desestilizado
+    // se a origem NAO tiver nenhum elemento, ao transitar para o outro card, eh preciso estilizar como card vazio
+    if (ulOrigParent != null)
+        if (ulOrigParent.childElementCount == 0)
+            modificarEstiloVazio(ulOrigParent, ulDestParent, true)
+    
+    // se o destino tiver apenas 1 elemento, ao transitar uma nova li para ele, eu preciso desestilaza-lo para receber o primeiro li
+    if (ulDestParent != null)
+        if (ulDestParent.childElementCount == 1) // aqui foi definido como 1, pois quando a funcao for chamada, ja vai existir 1 novo li
+            modificarEstiloVazio(ulOrigParent, ulDestParent, false)
+    
+}
+
+// Funcao auxiliar de verificarVazio() usada para add ou retirar os estilos dos cards
+function modificarEstiloVazio(ulOrigParent, ulDestParent, addEstilo) {
+    // add estilos de vazio
+    if (addEstilo) {
+        ulOrigParent.classList.add('card-list-container-empty') // modificar o estilo da UL para vazio
+        ulOrigParent.parentElement.parentElement.style.backgroundColor = "rgb(235, 235, 235)" // no card, escurecer o fundo
+        ulOrigParent.parentElement.parentElement.classList.add('cards-vazio') // no card, diminuir box-shadow e remove-lo no hover
+    } 
+    // retirar estilos de vazio e reverter as modificacoes feitas acima anteriormente
+    else {
+        ulDestParent.classList.remove('card-list-container-empty')
+        ulDestParent.parentElement.parentElement.style.backgroundColor = "white"
+        ulDestParent.parentElement.parentElement.classList.remove('cards-vazio')
     }
 }
